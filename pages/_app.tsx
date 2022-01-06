16337db3aee9dc13596f934ca9c047468bd3e183
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2022-01-06 14:21:43 +0100

    Font awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import '../styles/fontawesome_5.15.1_all.css'
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
